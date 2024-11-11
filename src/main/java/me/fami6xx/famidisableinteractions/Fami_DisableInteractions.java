5859f052b7dcdc29fff0cb3c41006ed8c837c9e8
Fami6Xx <libojer@gmail.com>
2024-11-11 20:49:04 +0100

    feat: init

---
+++ src/main/java/me/fami6xx/famidisableinteractions/Fami_DisableInteractions.java
@@ -0,0 +1,17 @@
+package me.fami6xx.famidisableinteractions;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Fami_DisableInteractions extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
